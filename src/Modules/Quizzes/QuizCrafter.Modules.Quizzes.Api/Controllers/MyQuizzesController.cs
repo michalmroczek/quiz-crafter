a59ef7e0ce4bab6a1fab7bea1899215d8463713b
Michał Mroczek <michal.mroczek@sympa.com>
2024-03-06 16:50:54 +0100

    Create new component and first module dummy api

---
+++ src/Modules/Quizzes/QuizCrafter.Modules.Quizzes.Api/Controllers/MyQuizzesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+
+namespace QuizCrafter.Modules.Quizzes.Api.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route($"{QuizzesModule.BasePath}/me/quizzes")]
+    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+    public class MyQuizzesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<QuizItemDto>>> GetQuizzesAsync()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<QuizDto>> GetQuizAsync()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<QuizDto>> CreateQuiz()
+        {
+            return Ok();
+        }
+
+    }
+    //To remove and create real dtos
+    public class QuizItemDto{}
+    public class QuizDto { }
+}
---
+++ src/Modules/Quizzes/QuizCrafter.Modules.Quizzes.Api/QuizzesModule.cs
@@ -0,0 +1,8 @@
+﻿namespace QuizCrafter.Modules.Quizzes.Api
+{
+    internal class QuizzesModule
+    {
+        public const string BasePath = "quizzes-module";
+        public string Name { get; } = "Quizzes";
+    }
+}
