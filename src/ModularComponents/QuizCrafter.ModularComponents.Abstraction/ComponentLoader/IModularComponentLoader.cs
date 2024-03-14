19813c317b04337096e786677dd37104e213d4c7
Michał Mroczek <michal.mroczek@sympa.com>
2024-03-14 17:11:18 +0100

    ui improvements

--- src/ModularComponents/QuizCrafter.ModularComponents.Abstraction/ComponentLoader/IModularComponentLoader.cs
+++ src/ModularComponents/QuizCrafter.ModularComponents.Abstraction/ComponentLoader/IModularComponentLoader.cs
@@ -1,9 +1,4 @@
 ﻿using QuizCrafter.ModularComponents.Abstraction.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace QuizCrafter.ModularComponents.Abstraction.ComponentLoader
 {
