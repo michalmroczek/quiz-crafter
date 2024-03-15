302f8769c71d02b35ae40e07b2555acbbddf82ab
Michał Mroczek <michal.mroczek@sympa.com>
2024-03-15 18:44:24 +0100

    improvements to pico components

---
+++ src/ModularComponents/QuizCrafter.ModularComponents.PlaceInCorrectOrder.Presentation/Models/AnswerItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizCrafter.ModularComponents.PlaceInCorrectOrder.Presentation.Models
+{
+    internal class AnswerItem
+    {
+        int Id { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ src/ModularComponents/QuizCrafter.ModularComponents.PlaceInCorrectOrder.Presentation/Models/Section.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizCrafter.ModularComponents.PlaceInCorrectOrder.Presentation.Models
+{
+    internal class Section
+    {
+        public int Id { get; }
+        public string Name { get; set; }
+
+        public List<AnswerItem> Answers { get; set; }
+    }
+}
