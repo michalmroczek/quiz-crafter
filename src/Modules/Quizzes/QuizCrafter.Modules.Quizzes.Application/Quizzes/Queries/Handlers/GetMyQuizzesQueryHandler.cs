fb0a595ed10430f776603e4f3addd82ed9005cde
Michał Mroczek <michal.mroczek@sympa.com>
2024-03-17 16:14:34 +0100

    add application layer and improvements

---
+++ src/Modules/Quizzes/QuizCrafter.Modules.Quizzes.Application/Quizzes/Queries/Handlers/GetMyQuizzesQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using QuizCrafter.Modules.Quizzes.Application.Quizzes.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizCrafter.Modules.Quizzes.Application.Quizzes.Queries.Handlers
+{
+    internal class GetMyQuizzesQueryHandler : IRequestHandler<GetMyQuizzesQuery, IEnumerable<QuizDto>>
+    {
+        public Task<IEnumerable<QuizDto>> Handle(GetMyQuizzesQuery request, CancellationToken cancellationToken)
+        {
+            return null;
+        }
+    }
+}
--- src/QuizCrafter.Web/Program.cs
+++ src/QuizCrafter.Web/Program.cs
@@ -1,5 +1,6 @@
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using QuizCrafter.ModularComponents.Infrastructure;
@@ -17,6 +18,20 @@ builder.Services.AddScoped<LocalizationMemoryStorage>();
 builder.Services.AddScoped<LazyCultureProvider>();
 builder.Services.AddScoped(typeof(IStringLocalizer<>), typeof(web.Services.StringLocalizer<>));
 */
+builder.Services.AddHttpClient("QuizCrafter.API", client => client.BaseAddress = new Uri(builder.Configuration["Api:Uri"]))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("QuizCrafter.API"));
+//builder.Services.AddScoped(sp => new HttpClient(
+//    sp.GetRequiredService<AuthorizationMessageHandler>()
+//    .ConfigureHandler(
+//        authorizedUrls: new[] { builder.Configuration["Api:Uri"] },
+//        scopes: new[] { "API.Access" }))
+//{
+//    BaseAddress = new Uri(builder.Configuration["Api:Uri"]),
+
+//});
+
 builder.Services.AddInfrastructure();
 builder.Services.AddMsalAuthentication(options =>
 {
@@ -24,6 +39,7 @@ builder.Services.AddMsalAuthentication(options =>
     options.ProviderOptions.DefaultAccessTokenScopes.Add("openid");
     options.ProviderOptions.DefaultAccessTokenScopes.Add("offline_access");
     options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://mmroczek.onmicrosoft.com/48244fe3-6a15-4996-a184-a4128b876903/API.Access");
 });
 
 await builder.Build().RunAsync();
