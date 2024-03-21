57a13789cc8a370706e395eecc7d14316398dcf2
Michał Mroczek <104060565+michalmroczek@users.noreply.github.com>
2024-03-21 22:10:15 +0100

    add ef core with mongo (#28)
    * add ef core with mongo
    
    * app settings

---
+++ src/Modules/Quizzes/QuizCrafter.Modules.Quizzes.Infrastructure/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using QuizCrafter.Modules.Quizzes.Domain.Quizzes.Repositories;
+using QuizCrafter.Modules.Quizzes.Infrastructure.EF;
+using QuizCrafter.Modules.Quizzes.Infrastructure.EF.Repositories;
+
+namespace QuizCrafter.Modules.Quizzes.Infrastructure
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddDbContext<QuizzesDbContext>(options =>
+            {
+                var client = new MongoClient("mongodb://localhost:27017");
+                var database = client.GetDatabase("QuizCrafter");
+                options.UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName);
+            });
+
+            services.AddScoped<IQuizRepository, QuizRepository>();
+
+            return services;
+        }
+    }
+}
+
+
+
