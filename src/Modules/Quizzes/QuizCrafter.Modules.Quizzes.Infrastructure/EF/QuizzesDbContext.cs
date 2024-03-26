96f7c7540c9f0b1669eeed274b48c93a2358030b
Michał Mroczek <104060565+michalmroczek@users.noreply.github.com>
2024-03-26 23:05:47 +0100

    add quiz creation (#33)

--- src/Modules/Quizzes/QuizCrafter.Modules.Quizzes.Infrastructure/EF/QuizzesDbContext.cs
+++ src/Modules/Quizzes/QuizCrafter.Modules.Quizzes.Infrastructure/EF/QuizzesDbContext.cs
@@ -20,21 +20,22 @@ namespace QuizCrafter.Modules.Quizzes.Infrastructure.EF
 
             modelBuilder.Entity<QuizItem>().ToCollection("quizzes");
 
-            modelBuilder.Entity<ModularComponentModel>().ToCollection("components")
+            var discriminatorBuilder = modelBuilder.Entity<ModularComponentModel>().ToCollection("components")
                 .HasDiscriminator<string>("Type");
 
 
             string assemblyPath = Assembly.GetExecutingAssembly().Location;
             var directoryAssemblies = Directory.GetFiles(Path.GetDirectoryName(assemblyPath), "QuizCrafter.ModularComponents.*.Presentation.dll")
                                                 .Select(Assembly.LoadFrom);
-            foreach ( var assembly in directoryAssemblies )
+            foreach (var assembly in directoryAssemblies)
             {
                 var componentTypes = assembly.GetTypes()
                 .Where(t => t.IsClass && !t.IsAbstract && t.IsSubclassOf(typeof(ModularComponentModel)));
 
                 foreach (var type in componentTypes)
                 {
-                    modelBuilder.Entity(type);
+                    // modelBuilder.Entity(type);
+                    discriminatorBuilder.HasValue(type, type.Name);
                 }
             }
 
